6b7e4b06ee9bf0f86011ceb25a649c52850b357c
norogoth <norogoth@gmail.com>
2021-07-14 13:56:28 -0700

    logo and subtitle

--- src/pages/index.js
+++ src/pages/index.js
@@ -12,12 +12,14 @@ const IndexPage = () => (
 	</head>
     <Seo title="Home" />
 	<div className="header-background-img-section"></div>	
+	<img className="logo" src="explogo.png" alt="EXP Realty"/>
 	<div className="content">
 		<h1 className="title">Karen Aaker Realty</h1>
+		<p className="subtitle">CRMLS - eXp Realty of California, Inc. CA DRE#01878277</p>
 		<div className="buttonDiv">
 			<div className="buttonDiv2">
-				<button type="button" class="btn btn-primary">I want to sell.</button>
-				<button type="button" class="btn btn-primary">I want to buy.</button>
+				<button type="button" class="btn btn-dark btn-lg">I want to sell.</button>
+				<button type="button" class="btn btn-dark btn-lg">I want to buy.</button>
 			</div>
 		</div>
 	</div>
